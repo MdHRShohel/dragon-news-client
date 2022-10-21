f750ccd1aaf082e1099db1a3ea4c37b0c5c7ff0b
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-22 05:17:23 +0600

    Carousel for brands advertisement added

--- src/Pages/Shared/BrandCarousel/BrandCarousel.js
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import Carousel from "react-bootstrap/Carousel";
+import Brand1 from "../../../assets/brands/Brand1.png";
+import Brand2 from "../../../assets/brands/Brand2.png";
 
 const BrandCarousel = () => {
     return (
       <div>
-        <Carousel>
+        <Carousel slide={false} controls={false} indicators={false}>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+            <img className="d-block w-100" src={Brand1} alt="First slide" />
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+            <img className="d-block w-100" src={Brand2} alt="Second slide" />
           </Carousel.Item>
         </Carousel>
       </div>
