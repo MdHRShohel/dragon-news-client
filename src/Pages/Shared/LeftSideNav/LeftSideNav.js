ca9616077d76612cd100fd0a3c5d4c680065082d
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-22 04:14:52 +0600

    Create Side Components with data from server

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h2>Left side nav</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/news-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <h4>All Category : {categories.length}</h4>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
