c7fc3a9e29b23dbc07a27a9c4e3e052735253ff4
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-23 20:53:58 +0600

    Display Login and Registration error

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,47 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
-
 const Login = () => {
-
   const { signIn } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [ error, setError ] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-
-        signIn(email, password)
-            .then((result) => {
-                const user = result.user;
-                console.log(user);
-                form.reset();
-                navigate('/');
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setError('');
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" required />
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" required />
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
       </Form.Group>
       <p className="text-danger">
-        
+        {error}
       </p>
       <Button variant="primary" type="submit">
         Login
--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,13 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Register = () => {
-
   const { createUser } = useContext(AuthContext);
-
-
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,44 +20,58 @@ const Register = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setError("");
         form.reset();
       })
       .catch((error) => {
-        console.log(error);
+        const errorMessage = error.message;
+        setError(errorMessage);
       });
   };
 
-    return (
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Your Full Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="Enter Your Full Name"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Photo URL</Form.Label>
-          <Form.Control type="text" name="photoURL" placeholder="Enter The URL of Your Photo" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name='email' placeholder="Enter email" required />
-        </Form.Group>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your Full Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="name"
+          placeholder="Enter Your Full Name"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Photo URL</Form.Label>
+        <Form.Control
+          type="text"
+          name="photoURL"
+          placeholder="Enter The URL of Your Photo"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
+      </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name='password' placeholder="Password" required />
-        </Form.Group>
-        <p className="text-danger">
-          
-        </p>
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-      </Form>
-    );
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+      </Form.Group>
+      <p className="text-danger">{error}</p>
+      <Button variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
