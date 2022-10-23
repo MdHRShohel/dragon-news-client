7c05ddec50022071079431795693c4d478770951
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-23 20:17:10 +0600

    login and registration implementation

--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+
+  const { createUser } = useContext(AuthContext);
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    //const name = form.name.value;
+    //const photoURL = form.photoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    //console.log(name, photoURL, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
     return (
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Your Full Name</Form.Label>
           <Form.Control
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {getAuth, onAuthStateChanged, signInWithPopup, signOut} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -12,6 +12,14 @@ const AuthProvider = ({children}) => {
     const providerLogin = (provider) => {
         return signInWithPopup(auth,provider);
     }
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
 
     const logOut = () => {
         return signOut(auth);
@@ -27,7 +35,7 @@ const AuthProvider = ({children}) => {
 
     }, []);
 
-    const authInfo = {user, providerLogin,logOut};
+    const authInfo = { user, providerLogin,logOut, createUser, signIn };
     
     return (
         <AuthContext.Provider value={authInfo}>
